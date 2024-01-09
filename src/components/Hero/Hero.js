955118b29b3e52b2b3a477c95e395b4a02689e9f
LanaAlrayes <lanaalrayss@gmail.com>
2024-01-09 11:57:26 +0300

    add Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,15 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className='hero-main'>
+            <div className="hero-text">
+                <h6 className="hero-subtitle">Welcome To Cyborg</h6>
+                <h4 className="hero-title"><em>Browse</em> Our Popular Games Here</h4>
+                <div className="main-button">
+                    <a href="browse.html">Browse Now</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
