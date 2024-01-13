e14f98330db4f4531812d7f47a95f03d6715f8be
LanaAlrayes <lanaalrayss@gmail.com>
2024-01-13 20:46:12 +0300

    add Gaming Library Data

---
+++ src/Data/GamingLibraryData.js
@@ -0,0 +1,10 @@
+import GamingLibrary_01 from '../assets/images/game_01.jpg'
+import GamingLibrary_02 from '../assets/images/game_02.jpg'
+import GamingLibrary_03 from '../assets/images/game_03.jpg'
+
+const GamingLibraryData = [
+    { id: "library_item_0", image: GamingLibrary_01, title: "Fortnite", category: "Sandbox", date_added: "24/08/2021", hours_played: "634 H 22 Mins", download: "Downloaded" },
+    { id: "library_item_1", image: GamingLibrary_02, title: "CS-GO", category: "Sandbox", date_added: "24/08/2021", hours_played: "634 H 22 Mins", download: "Downloaded" },
+    { id: "library_item_2", image: GamingLibrary_03, title: "Dota 2", category: "Sandbox", date_added: "24/08/2021", hours_played: "634 H 22 Mins", download: "Downloaded" },
+]
+export default GamingLibraryData
