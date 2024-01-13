1a1dd9815ac03df08ca8fc5a000889615d444ca1
LanaAlrayes <lanaalrayss@gmail.com>
2024-01-13 16:04:07 +0300

    use array

--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -1,32 +1,22 @@
 import './MostPopular.css'
-import { Card } from '../../components/index'
-import popular_01 from '../../assets/images/popular_01.jpg'
-import popular_02 from '../../assets/images/popular_02.jpg'
-import popular_03 from '../../assets/images/popular_03.jpg'
-import popular_04 from '../../assets/images/popular_04.jpg'
-import popular_05 from '../../assets/images/popular_05.jpg'
-import popular_06 from '../../assets/images/popular_06.jpg'
-import popular_07 from '../../assets/images/popular_07.jpg'
-import popular_08 from '../../assets/images/popular_08.jpg'
+import { Card, SectionHeader, SectionWrapper } from '../../components/index'
+import MostPopularData from '../../Data/MostPopularData'
+
 
 const MostPopular = () => {
-    return (
-        <div className='section-wrapper'>
-            <div className='section-header'>
-                <h4>Most Popular</h4>
-            </div>
-            <div className='most-popular-items'>
-                <Card image={popular_01} title="Fortnite" category="Sandbox" rate="3.8" download="2.3M" />
-                <Card image={popular_02} title="Pubg" category="Stream-X" rate="4.8" download="1.1M" />
-                <Card image={popular_03} title="Dota2" category="Legendary" rate="2.7" download="5.3M" />
-                <Card image={popular_04} title="CS-Go" category="Battle S" rate="3.9" download="6.1M" />
-                <Card image={popular_05} title="MiniCraft" category="Sandbox" rate="3.8" download="2.3M" />
-                <Card image={popular_06} title="Eagles Fly" category="Matrix Games" rate="4.8" download="1.1M" />
-                <Card image={popular_07} title="Warface" category="Max 3D" rate="2.7" download="5.3M" />
-                <Card image={popular_08} title="Warcraft" category="Legend" rate="3.9" download="6.1M" />
+    const cards = MostPopularData.map(card => {
+        return <Card key={card.id} image={card.image} title={card.title} category={card.category} rate={card.rate} download={card.download} />
+    })
 
-            </div>
-        </div>
+    return (
+        <>
+            <SectionWrapper>
+                <SectionHeader> Most Popular </SectionHeader>
+                <div className='most-popular-items'>
+                    {cards}
+                </div>
+            </SectionWrapper>
+        </>
     )
 }
 
